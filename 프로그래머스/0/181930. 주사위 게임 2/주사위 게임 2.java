e8f32700222b031ec763d02326673f96c4e9f34e
이다은 <132417166+leeeunda@users.noreply.github.com>
2025-02-28 21:42:37 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,13 +1,12 @@
 public class Solution {
     public int solution(int a, int b, int c) {
-        if(a == b && b == c) {
-            return 27 * a * a * a * a * a * a;
-        } else if(a == b || a == c || b == c) {
-            int sum = a + b + c;
-            int sumSquare = a * a + b * b + c * c;
-            return sum * sumSquare;
-        } else {
-            return a + b + c;
+        int answer = a+b+c;
+        
+        if (a==b && b==c){
+            answer*=(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        } else if(a==b||b==c||a==c){
+            answer*=(a*a+b*b+c*c);
         }
+        return answer;
     }
 }
