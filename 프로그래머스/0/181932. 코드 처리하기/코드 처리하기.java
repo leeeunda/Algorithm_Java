1e7c7064cb674addaf57f5e730d643f34026316b
이다은 <132417166+leeeunda@users.noreply.github.com>
2025-02-28 21:07:25 +0900

    [level 0] Title: 코드 처리하기, Time: 6.55 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -4,13 +4,13 @@ class Solution {
         int mode = 0;
         
         for (int i = 0; i < code.length(); i++) {
-            char c = code.charAt(i);
-            if (c == '1') {
-                mode = 1 - mode; 
-            } else if ((mode == 0 && i % 2 == 0) || (mode == 1 && i % 2 == 1)) {
+            char c=code.charAt(i);
+            if (c=='1'){
+                mode=1-mode;
+            } else if (i%2==mode){
                 ret.append(c);
             }
-        }
-        return ret.length() == 0 ? "EMPTY" : ret.toString();
+    }
+        return ret.length()==0?"EMPTY":ret.toString();
     }
 }
