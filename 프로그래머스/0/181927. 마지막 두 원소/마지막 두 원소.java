5a580b56ada04fc0b161ebc92b5c4468b00bc319
이다은 <132417166+leeeunda@users.noreply.github.com>
2025-03-02 23:59:06 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,14 +1,6 @@
-public class Solution {
+class Solution {
     public int[] solution(int[] num_list) {
-        int size = num_list.length;
-        int last = num_list[size - 1];
-        int secondLast = num_list[size - 2];
-        int newElement = last > secondLast ? last - secondLast : last * 2;
-
-        int[] answer = new int[size + 1];
-        System.arraycopy(num_list, 0, answer, 0, size);
-        answer[size] = newElement;
-
-        return answer;
+        int n=num_list.length-1;
+        return num_list[n] > num_list[n-1] ? num_list[n]-num_list[n-1]:2*num_list[n];
     }
 }
